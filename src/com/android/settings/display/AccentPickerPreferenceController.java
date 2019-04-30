DirtyUnicorns/android_packages_apps_Settings

Fruity Pebbles v2.0
Change-Id: I0528a51794c65ad0873f2a8ac22cf1c373f86967

src/com/android/settings/display/AccentPickerPreferenceController.java
@@ -32,10 +32,14 @@ import com.android.settingslib.core.lifecycle.events.OnResume;
 
 import com.dirtyunicorns.tweaks.fragments.AccentPicker;
 
+import com.android.internal.util.du.Utils;
+
 public class AccentPickerPreferenceController extends AbstractPreferenceController
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private final Fragment mParent;
@@ -52,6 +56,7 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+        mAccentPickerPref.setEnabled(true);
     }
 
     @Override
@@ -62,7 +67,7 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
 
     @Override
     public boolean isAvailable() {
-        return true;
+        return !Utils.isPackageInstalled(mContext, SUBS_PACKAGE);
     }
 
     @Override
@@ -70,25 +75,25 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
         return KEY_ACCENT_PICKER_FRAGMENT_PREF;
     }
 
-    public void updateEnableState() {
+    private void updateEnableState() {
         if (mAccentPickerPref == null) {
             return;
         }
 
         mAccentPickerPref.setOnPreferenceClickListener(
-            new OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    AccentPicker.show(mParent);
-                    return true;
-                }
-            });
+                new OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        AccentPicker.show(mParent);
+                        return true;
+                    }
+                });
     }
 
-    public void updateSummary() {
+    private void updateSummary() {
         if (mAccentPickerPref != null) {
-                mAccentPickerPref.setSummary(mContext.getString(
-                        com.android.settings.R.string.theme_accent_picker_summary));
+            mAccentPickerPref.setSummary(mContext.getString(
+                    com.android.settings.R.string.theme_accent_picker_summary));
         }
     }
 }

src/com/android/settings/display/DarkUIPreferenceController.java
@@ -14,32 +14,35 @@
  * limitations under the License
  */
 
-package com.android.settings.development;
+package com.android.settings.display;
 
 import android.app.UiModeManager;
 import android.content.Context;
-import android.support.annotation.VisibleForTesting;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 
 import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
-import com.android.settingslib.development.DeveloperOptionsPreferenceController;
 
-public class DarkUIPreferenceController extends DeveloperOptionsPreferenceController
+import com.android.internal.util.du.Utils;
+
+public class DarkUIPreferenceController extends AbstractPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
 
     private static final String DARK_UI_KEY = "dark_ui_mode";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private final UiModeManager mUiModeManager;
 
     public DarkUIPreferenceController(Context context) {
-        this(context, context.getSystemService(UiModeManager.class));
+        super(context);
+        mUiModeManager = context.getSystemService(UiModeManager.class);
     }
 
-    @VisibleForTesting
-    DarkUIPreferenceController(Context context, UiModeManager uiModeManager) {
-        super(context);
-        mUiModeManager = uiModeManager;
+    @Override
+    public boolean isAvailable() {
+        return !Utils.isPackageInstalled(mContext, SUBS_PACKAGE);
     }
 
     @Override

nysascape/platform_packages_apps_Settings

Signed-off-by: mracar07 <umutcan@umutcanacar.me>

src/com/android/settings/display/AccentPickerPreferenceController.java
@@ -32,10 +32,14 @@ import com.android.settingslib.core.lifecycle.events.OnResume;
 
 import com.dirtyunicorns.tweaks.fragments.AccentPicker;
 
+import com.android.internal.util.aosap.Utils;
+
 public class AccentPickerPreferenceController extends AbstractPreferenceController
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private final Fragment mParent;
@@ -52,6 +56,7 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+        mAccentPickerPref.setEnabled(true);
     }
 
     @Override
@@ -62,7 +67,7 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
 
     @Override
     public boolean isAvailable() {
-        return true;
+        return !Utils.isPackageInstalled(mContext, SUBS_PACKAGE);
     }
 
     @Override
@@ -70,25 +75,25 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
         return KEY_ACCENT_PICKER_FRAGMENT_PREF;
     }
 
-    public void updateEnableState() {
+    private void updateEnableState() {
         if (mAccentPickerPref == null) {
             return;
         }
 
         mAccentPickerPref.setOnPreferenceClickListener(
-            new OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    AccentPicker.show(mParent);
-                    return true;
-                }
-            });
+                new OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        AccentPicker.show(mParent);
+                        return true;
+                    }
+                });
     }
 
-    public void updateSummary() {
+    private void updateSummary() {
         if (mAccentPickerPref != null) {
-                mAccentPickerPref.setSummary(mContext.getString(
-                        com.android.settings.R.string.theme_accent_picker_summary));
+            mAccentPickerPref.setSummary(mContext.getString(
+                    com.android.settings.R.string.theme_accent_picker_summary));
         }
     }
 }

src/com/android/settings/display/DarkUIPreferenceController.java
@@ -14,32 +14,35 @@
  * limitations under the License
  */
 
-package com.android.settings.development;
+package com.android.settings.display;
 
 import android.app.UiModeManager;
 import android.content.Context;
-import android.support.annotation.VisibleForTesting;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 
 import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
-import com.android.settingslib.development.DeveloperOptionsPreferenceController;
 
-public class DarkUIPreferenceController extends DeveloperOptionsPreferenceController
+import com.android.internal.util.aosap.Utils;
+
+public class DarkUIPreferenceController extends AbstractPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
 
     private static final String DARK_UI_KEY = "dark_ui_mode";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private final UiModeManager mUiModeManager;
 
     public DarkUIPreferenceController(Context context) {
-        this(context, context.getSystemService(UiModeManager.class));
+        super(context);
+        mUiModeManager = context.getSystemService(UiModeManager.class);
     }
 
-    @VisibleForTesting
-    DarkUIPreferenceController(Context context, UiModeManager uiModeManager) {
-        super(context);
-        mUiModeManager = uiModeManager;
+    @Override
+    public boolean isAvailable() {
+        return !Utils.isPackageInstalled(mContext, SUBS_PACKAGE);
     }
 
     @Override